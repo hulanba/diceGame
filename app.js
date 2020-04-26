3cef0c61a73d68ef4003d0805af73a75d61cd66d
Khulan <hulanba3@gmail.com>
2020-04-26 18:14:32 +0800

    eelj solij onoog roundScored tsugluuldag bolloo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тогогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -15,9 +15,36 @@ document.getElementById("cs-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 // click хийхэд ажиллах event listener
-
 document.querySelector(".roll").addEventListener("click", function () {
+  //1-6 хүртэл санамсаргүй тоо
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //шооны зургийг гаргаж ирэх
   diceDom.style.display = "block";
+  //санамсаргүй утгат харгалзах зургийг гаргаж ирэх
   diceDom.src = "./img/dice-" + diceNumber + ".png";
+
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("cs-" + activePlayer).textContent = roundScore;
+  } else {
+    //элээд ээлжйн оноог 0 болгох
+    document.getElementById("cs-" + activePlayer).textContent = 0;
+    roundScore = 0;
+    // дараа нь 1 буусан тохиолдолд ээлжийг солино
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    //Active player-г өөрчлөх
+    document.querySelector(".wrap-1").classList.toggle("active");
+    document.querySelector(".wrap-2").classList.toggle("active");
+
+    //Шоог түр алга болгох
+
+    diceDom.style.display = "none";
+
+    /*if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }*/
+  }
 });
