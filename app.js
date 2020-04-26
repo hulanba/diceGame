b60ccf075c2d07d27811c8bc49a07e5d636c6ed8
Khulan <hulanba3@gmail.com>
2020-04-26 17:18:38 +0800

    shoo shiddeg bollo

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ var scores = [0, 0];
 //Ээлжийн оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шооны аль талаараа буусаныг хадгалах хувьсагч 1-6 гэсэн утгыг санамсаргүйгээр үүсгэх
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#score-0").textContent = dice;
-document.querySelector(".sc-0").textContent = 0;
-document.querySelector(".sc-1").textContent = 0;
-document.querySelector("#score-1").innerHTML = "<em>Yes!</em>";
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("cs-0").textContent = "0";
+document.getElementById("cs-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 // click хийхэд ажиллах event listener
 
-document.querySelector;
+document.querySelector(".roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "./img/dice-" + diceNumber + ".png";
+});
